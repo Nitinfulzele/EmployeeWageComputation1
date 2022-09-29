d2a240d1527129aaf411ba9eeaefa6be5ab8ac93
Nitinfulzele <nitinfulzele99@gmail.com>
2022-09-30 00:40:05 +0530

    Added Daily Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,16 +4,26 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 	
+	static int wagePerHour = 20;
+	static int fullDayHour = 8;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation ");
          Random inp = new Random();
          int isPresent = inp.nextInt(2);
+         
          if (isPresent == 1) {
         	 System.out.println("Employee is Present");
          }
          else {
         	 System.out.println("Employee is Absent");
          }
+         
+         int dailyWage=0;
+         dailyWage = fullDayHour*wagePerHour;
+         System.out.println("Daily wage of Employee is " + dailyWage);
 	}
 
 }
+
+
