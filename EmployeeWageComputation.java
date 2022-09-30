b4f4a0b70b4f942838291aaafda9fa2bb3e43356
Nitinfulzele <nitinfulzele99@gmail.com>
2022-09-30 16:02:19 +0530

    Monthly wage added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -18,35 +18,46 @@ public class EmployeeWageComputation {
 		} else {
 			System.out.println("Employee is Absent");
 		}
- 
-		int dailyWage = 0;
-	    dailyWage = fullDayHour*wagePerHour;
-		dailyWage = partTimeHour*wagePerHour;
-    
-		System.out.println("Daily wage of Employee is " + dailyWage); 
-		
-		dailyWage = fullDayHour* 10;
-	    System.out.println("Employee is Part time");
-	    System.out.println("Part Time Employee daily wage is " + dailyWage +"Rs");
-         
-	    isPresent = inp.nextInt(2);
 
+		// calculate daily employee wage
+
+		int dailyWage = 0;
+		int dailyWage2 = 0;
+		
+		dailyWage = fullDayHour * wagePerHour;
+		dailyWage = partTimeHour * wagePerHour;
+
+		System.out.println("Daily wage of Employee is " + dailyWage);
+
+		dailyWage2 = partTimeHour * 10;
+		System.out.println("Employee is Part time");
+		System.out.println("Part Time Employee daily wage is " + dailyWage2 + " Rs ");
+
+		// solved using switch case
+		isPresent = inp.nextInt(2);
 		switch (isPresent) {
 		case 0:
-		    System.out.println("Employee is Present");
+			System.out.println("Employee is Present");
 			break;
 		case 1:
 			System.out.println("Employee is Part Time");
-	
 			break;
-			
 		case 2:
 			System.out.println("Employee is Absent");
 			break;
-			
-			
+
 		}
 
+		// Calculate monthly wage
+
+		int monthlyWage = 0;
+		int monthDays = 20;
+
+		monthlyWage = dailyWage * monthDays;
+		System.out.println("Monthly wage of Employee is " + monthlyWage);
+
+		monthlyWage = dailyWage2 * monthDays;
+		System.out.println("Monthly wage of part time Employee is " + monthlyWage);
 	}
 
 }
